00837d28ba6426d5eb5dc18bfb904c4fce793bc6
lolipuru <banstellazz@gmail.com>
2023-07-27 03:08:25 +0700

    sm8550-common: build with permissive for now
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -112,7 +112,8 @@ BOARD_KERNEL_CMDLINE := \
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
-    androidboot.usbcontroller=a600000.dwc3
+    androidboot.usbcontroller=a600000.dwc3 \
+    androidboot.selinux=permissive
 
 BOARD_USES_GENERIC_KERNEL_IMAGE := true
 BOARD_KERNEL_BASE := 0x00000000
